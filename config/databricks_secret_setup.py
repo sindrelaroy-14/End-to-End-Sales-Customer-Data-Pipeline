8aa6469cdb2732cb0dd306439ad4862fb377ab16
sindrelaroy-14 <roysindrela@gmail.com>
2026-01-06 18:33:36 +0000

    Created the bronze layer

---
+++ config/databricks_secret_setup.py
@@ -0,0 +1,13 @@
+import urllib.parse
+
+# Use a dictionary to keep everything organized
+AWS_CREDS = {
+    "ACCESS_KEY": "********",
+    "SECRET_KEY": "########",
+    "BUCKET": "s3_bucket"
+}
+
+def get_s3_base_path():
+    encoded_secret = urllib.parse.quote(AWS_CREDS["SECRET_KEY"], safe="")
+    return f"s3a://{AWS_CREDS['ACCESS_KEY']}:{encoded_secret}@{AWS_CREDS['BUCKET']}/landing-zone/"
+
